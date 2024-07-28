571eab4859e7305f0f775d793d6fc23beb46c83c
@priti.grgn <priti.grgn@gmail.com>
2024-07-28 19:17:38 +0545

    userauth

--- base/views.py
+++ base/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
+
 
 # Create your views here.
 
@@ -11,6 +15,30 @@ from .forms import RoomForm
 #     {'id':3, 'name':'frontend developers'},
     
 # ]
+
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User doesnt exist')
+        
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password doesnt exist')
+        
+    context= {}
+    return render(request, 'base/login_register.html',context)
+
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') !=None else ''
