39acbaf7fc3cc582a70e48231a1d97ebd3ea9467
@priti.grgn <priti.grgn@gmail.com>
2024-08-09 21:28:29 +0545

    templating

--- base/views.py
+++ base/views.py
@@ -77,15 +77,17 @@ def home(request):
 
     topic = Topic.objects.all()
     room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
-    context = {'rooms' : rooms, 'topics': topic,'room_count' : room_count}
+    context = {'rooms' : rooms, 'topics': topic,'room_count' : room_count,
+               'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 
 def room(request,pk):
   
     room= Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants = room.participants.all()
     if request.method == 'POST':
         message = Message.objects.create(
@@ -100,6 +102,16 @@ def room(request,pk):
 
     return render(request, 'base/room.html', context)
 
+def userProfile(request,pk):
+    user= User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user ,'rooms':rooms,
+               'room_messages' : room_messages, 'topics' : topics}
+    return render(request,'base/profile.html',context)
+
+
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
@@ -109,7 +121,9 @@ def createRoom(request):
     if request.method == 'POST' :
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit = False)
+            room.host = request.user
+            room.save()
             return redirect('home')
         
     context = {'form' : form}
