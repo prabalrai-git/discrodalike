9c3158ec30eb72547b1daf677960578b9946cd58
Prabal Rai <Prabalrai17@gmail.com>
2024-09-06 12:38:41 +0545

    initial commit

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+echo "Setting up a virtual environment"
+python3 -m venv venv
+source venv/bin/activate
+
+echo "Upgrading pip"
+python3 -m pip install --upgrade pip
+
 echo "Installing dependencies..."
 python3 -m pip install -r requirements.txt
 
