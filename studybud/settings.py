c502f9775b9dd3c71c2189dd4dbfdba9a20211d5
Prabal Rai <Prabalrai17@gmail.com>
2024-09-06 12:19:31 +0545

    initial commit

--- studybud/settings.py
+++ studybud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lttqzvaxo#ojg(@@02)8qy7uxt5t=d7oce8x$&y_=8hz)!9vam
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
 
 
 # Application definition
@@ -130,3 +130,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
--- studybud/urls.py
+++ studybud/urls.py
@@ -8,3 +8,5 @@ urlpatterns = [
     path('',include('base.urls'))
    
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
