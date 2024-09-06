249fe4ec565ad7612b92bf385dbe053c1065e7e5
Prabal Rai <Prabalrai17@gmail.com>
2024-09-06 12:26:58 +0545

    initial commit

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,12 @@
 # build_files.sh
+echo 'Building project packages...'
+python3 -m pip install -r requirements.txt
 
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+
+echo 'Migrating database...'
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+
+echo 'Collecting static files...'
+python3 manage.py collectstatic --noinput
